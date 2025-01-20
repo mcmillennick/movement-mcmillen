00993b4aaafa6718e1a9941e957dba8d80aa7f0d
Nicholas McMillen <nicholasflintwillow@gmail.com>
2025-01-20 06:07:10 -0800

    ci: added conventional commit check script

--- scripts/ops/conv-commit-check.sh
+++ scripts/ops/conv-commit-check.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo "Creating semantic release"
+echo "Checking for conventional commits"
 
 # get current commit hash
 COMMIT_HASH_CURRENT=$(git rev-parse HEAD)
-echo "Current commit hash: $COMMIT_HASH_CURRENT"
 
 # get the last tag on the current branch
 LAST_TAG=$(git describe --tags --abbrev=0)
-echo "Last tag: $LAST_TAG"
 
 # get the commit hash of the last tag
 COMMIT_HASH_LAST_TAG=$(git rev-list -n 1 $LAST_TAG)
-echo "Last tag commit hash: $COMMIT_HASH_LAST_TAG"
 
 # check if the current commit hash is the same as the last tag commit hash
 if [ "$COMMIT_HASH_CURRENT" == "$COMMIT_HASH_LAST_TAG" ]; then
     echo "No new commits since the last tag"
-    exit 1
+    exit 0
 fi
 
 echo "Checking for conventional commits"
 
 # generate changelog
-convco check $COMMIT_HASH_LAST_TAG..$COMMIT_HASH_CURRENT
+CONVENTIONAL_COMMITS=$(convco check $COMMIT_HASH_LAST_TAG..$COMMIT_HASH_CURRENT)
 
-# bump version
-echo "Bumping version"
-cog bump --auto
+echo "Conventional commits: $CONVENTIONAL_COMMITS"
+
+if [ -z "$CONVENTIONAL_COMMITS" ]; then
+    echo "No conventional commits found"
+    exit 1
+fi
